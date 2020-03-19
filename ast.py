a64a1e7aef5a1dfa410222731b57ebbff091e9b3
Krystof Rehacek <krystofee@gmail.com>
2020-03-20 00:02:12 +0100

    WIP 5.3

--- ast.py
+++ ast.py
@@ -1,24 +1,13 @@
-from typing import List
+from enum import Enum
 
 
 class Expr:
-    __init_args: List[str]
+    class Type(Enum):
+        BINARY = 'BINARY'
+        GROUPING = 'GROUPING'
+        LITERAL = 'LITERAL'
+        UNARY = 'UNARY'
 
-    Binary: 'Expr'
-    Grouping: 'Expr'
-    Literal: 'Expr'
-    Unary: 'Expr'
-
-    def __init__(self, *args)
-        for name, value in zip(self.__init_args, args):
-            setattr(self, name, value)
-
-    @staticmethod
-    def create_class(name, *args):
-        setattr(Expr, name, type(name, (Expr,), {'__init_args': args}))
-
-
-Expr.create_class('Binary', 'left', 'operator', 'right')
-Expr.create_class('Grouping', 'expression')
-Expr.create_class('Literal', 'value')
-Expr.create_class('Unary', 'operator', 'right')
+    def __init__(self, type: Type, *args):
+        self.type = type
+        self.args = args
